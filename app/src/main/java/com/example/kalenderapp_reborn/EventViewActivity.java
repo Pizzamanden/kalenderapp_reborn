7b47b09da2ed1b0ab7f9e3ad92bda4bec08a8aaf
Peter Winther <dialga399@live.dk>
2019-02-25 15:28:20 +0100

    Commit, not in working condition (check boolean arraylist (json booleans are 1 and 0))

--- app/src/main/java/com/example/kalenderapp_reborn/EventViewActivity.java
+++ app/src/main/java/com/example/kalenderapp_reborn/EventViewActivity.java
@@ -1,10 +1,22 @@
 package com.example.kalenderapp_reborn;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.example.kalenderapp_reborn.adapters.ViewEventRecyclerAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -17,19 +29,29 @@ import okhttp3.Response;
 public class EventViewActivity extends AppCompatActivity {
 
     private static final String TAG = "EventViewActivity";
-
+    private Context mContext;
     private String mJSONdata;
+
+    private String[] stringArrayMonths;
+    private String[] stringArrayWeekDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view);
 
+        // Setup Views
+        mContext = this;
+
+        stringArrayMonths = getResources().getStringArray(R.array.monthss);
+        stringArrayWeekDays = getResources().getStringArray(R.array.weekDaysS);
+
         assert getSupportActionBar() != null;
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+
         getDataJSON();
-        Log.d(TAG, "onCreate: " + mJSONdata);
     }
 
 
@@ -79,7 +101,7 @@ public class EventViewActivity extends AppCompatActivity {
                     EventViewActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            mJSONdata = myResponse;
+                            initRecyclerView(myResponse);
                         }
                     });
                 }
@@ -91,5 +113,85 @@ public class EventViewActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp(){
         finish();
         return true;
+    }
+
+    private void initRecyclerView(String jsonString){
+        Log.d(TAG, "initRecyclerView: " + jsonString);
+
+        ArrayList<String> eventNames = new ArrayList<>();
+        ArrayList<String> eventStart = new ArrayList<>();
+        ArrayList<String> eventEnd = new ArrayList<>();
+        ArrayList<Boolean> eventAlarmStatus = new ArrayList<>();
+        ArrayList<String> eventAlarmTime = new ArrayList<>();
+        ArrayList<Integer> eventIds = new ArrayList<>();
+
+        // Setup arraylists for recyclerview
+        try {
+            JSONArray mJSONArray = new JSONArray(jsonString);
+            for(int i = 0;i<mJSONArray.length(); i++){
+                JSONObject json = mJSONArray.getJSONObject(i);
+                eventNames.add(json.getString("event_name"));
+                eventStart.add(phpTimeToAndroid(json.getInt("event_start")));
+                eventEnd.add(phpTimeToAndroid(json.getInt("event_end")));
+                eventAlarmStatus.add(json.getBoolean("event_alarmenable"));
+                Log.d(TAG, "initRecyclerView: " + json.getBoolean("event_alarmenable"));
+                eventAlarmTime.add(convertAlarmTimeToString(json.getBoolean("event_alarmenable"), json.getInt("event_end"), json.getInt("event_start")));
+                eventIds.add(json.getInt("post_id"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_viewEvents);
+        ViewEventRecyclerAdapter adapter = new ViewEventRecyclerAdapter(mContext, eventNames, eventStart, eventEnd, eventIds, eventAlarmStatus, eventAlarmTime);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(mContext);
+        recyclerView.setLayoutManager(mLayoutManager);
+    }
+
+    // Method job: Create a stitched date-string to put into views, using only an epoch-time(int/long)
+    // Accepts: Takes an int that should function as an unix epoch time unit, either milliseconds or just seconds
+    public String phpTimeToAndroid(long epochTime){
+        Calendar calToday = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance();
+        Date mDate;
+        int length = String.valueOf(epochTime).length();
+        if(length < 12){
+            mDate = new Date(epochTime * 1000);
+        } else {
+            mDate = new Date(epochTime);
+        }
+        calendar.setTime(mDate);
+        String dateString = "";
+        // Week day as 3 letters
+        dateString += stringArrayWeekDays[calendar.get(Calendar.DAY_OF_WEEK)] + " ";
+        // Day of the month
+        if(calendar.get(Calendar.DAY_OF_MONTH)<10){
+            dateString += "0";
+        }
+        dateString += calendar.get(Calendar.DAY_OF_MONTH);
+        // Month as 3 letters
+        dateString += ". " + stringArrayMonths[calendar.get(Calendar.MONTH)] + ", ";
+        // Time as HH/MM
+        // Hours
+        if(calendar.get(Calendar.HOUR)<10){
+            dateString += "0";
+        }
+        dateString += calendar.get(Calendar.HOUR) + ":";
+        // Minutes
+        if(calendar.get(Calendar.MINUTE)<10){
+            dateString += "0";
+        }
+        dateString += calendar.get(Calendar.MINUTE);
+        if(calendar.get(Calendar.YEAR) != calToday.get(Calendar.YEAR)){
+            dateString += " - " + calendar.get(Calendar.YEAR);
+        }
+        return dateString;
+    }
+
+    public String convertAlarmTimeToString(Boolean isSet, long alarmTime, long startTime){
+        Log.d(TAG, "convertAlarmTimeToString: isSet: " + isSet);
+        Log.d(TAG, "convertAlarmTimeToString: alarmTime: " + alarmTime);
+        Log.d(TAG, "convertAlarmTimeToString: startTime: " + startTime);
+        return "Klokken yaads";
     }
 }
--- app/src/main/java/com/example/kalenderapp_reborn/adapters/ViewEventRecyclerAdapter.java
+++ app/src/main/java/com/example/kalenderapp_reborn/adapters/ViewEventRecyclerAdapter.java
@@ -1,15 +1,43 @@
 package com.example.kalenderapp_reborn.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.kalenderapp_reborn.R;
 
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
 public class ViewEventRecyclerAdapter extends RecyclerView.Adapter<ViewEventRecyclerAdapter.ViewHolder> {
+
+    final static private String TAG = "ViewEventRecycler";
+    Context mContext;
+    private JSONArray mJSONArray;
+    private ArrayList<String> eventTitle;
+    private ArrayList<String> eventStart;
+    private ArrayList<String> eventEnd;
+    private ArrayList<Boolean> eventAlarmStatus;
+    private ArrayList<String> eventAlarmTime;
+    private ArrayList<Integer> eventId;
+
+    public ViewEventRecyclerAdapter(Context context, ArrayList<String> titles, ArrayList<String> timeends, ArrayList<String> timestarts, ArrayList<Integer> ids, ArrayList<Boolean> alarmstatus, ArrayList<String> alarmtime){
+        mContext = context;
+        eventTitle = titles;
+        eventStart = timestarts;
+        eventEnd = timeends;
+        eventId = ids;
+        eventAlarmStatus = alarmstatus;
+        eventAlarmTime = alarmtime;
+    }
 
     @NonNull
     @Override
@@ -19,25 +47,55 @@ public class ViewEventRecyclerAdapter extends RecyclerView.Adapter<ViewEventRecy
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
+        viewHolder.textView_title.setText(eventTitle.get(i));
+        viewHolder.textView_timestart.setText(eventStart.get(i));
+        viewHolder.textView_timeend.setText(eventEnd.get(i));
+        if(eventAlarmStatus.get(i)){
+            viewHolder.textView_statusalarm.setText("Yes, ");
+            viewHolder.textView_timealarm.setText(eventAlarmTime.get(i));
+        } else {
+            viewHolder.textView_statusalarm.setText("No");
+            viewHolder.textView_timealarm.setText("");
+        }
+        viewHolder.imageView_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Edit " + eventId.get(viewHolder.getAdapterPosition()));
+            }
+        });
+        viewHolder.imageView_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Delete " + eventId.get(viewHolder.getAdapterPosition()));
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return eventTitle.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
 
+    public class ViewHolder extends RecyclerView.ViewHolder {
         // Declare views
-        ConstraintLayout parent_layout;
+        ConstraintLayout constraintLayout_parent;
+        ImageView imageView_edit, imageView_delete;
+        TextView textView_title, textView_timestart, textView_timeend, textView_statusalarm, textView_timealarm;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
             // Setup views
-
+            constraintLayout_parent = itemView.findViewById(R.id.constraintLayout_parent);
+            textView_title = itemView.findViewById(R.id.textView_title);
+            textView_timestart = itemView.findViewById(R.id.textView_timestart);
+            textView_timeend = itemView.findViewById(R.id.textView_timeend);
+            textView_statusalarm = itemView.findViewById(R.id.textView_statusalarm);
+            textView_timealarm = itemView.findViewById(R.id.textView_timealarm);
+            imageView_edit = itemView.findViewById(R.id.imageView_edit);
+            imageView_delete = itemView.findViewById(R.id.imageView_delete);
         }
     }
 }
